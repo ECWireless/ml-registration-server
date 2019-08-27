9890ffe769761aaae0b67c02e22ea24d7f8702c1
ECWireless <Elliott@valtechcreative.com>
2019-08-27 13:54:22 -0400

    adding graphql

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// const { buildSchema } = require('graphql');
-// const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const graphqlHttp = require('express-graphql');
 
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -17,54 +17,54 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port);
-
 // Arrays
 
 // let forms = [];
 
-// app.use('/graphql', graphqlHttp({
-//     schema: buildSchema(`
-//         type Form {
-//             _id: ID!
-//             name: String!
-//             phoneNumber: String!
-//             email: String!
-//         }
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type Form {
+            _id: ID!
+            name: String!
+            phoneNumber: String!
+            email: String!
+        }
 
-//         input FormInput {
-//             name: String!
-//             phoneNumber: String!
-//             email: String!
-//         }
+        input FormInput {
+            name: String!
+            phoneNumber: String!
+            email: String!
+        }
 
-//         type RootQuery {
-//             users: [String!]
-//             forms: [Form!]!
-//         }
-//         type RootMutation {
-//             createForm(formInput: FormInput): Form
-//         }
-//         schema {
-//             query: RootQuery
-//             mutation: RootMutation
-//         }
-//     `),
-//     rootValue: {
-//         forms: () => {
-//             return forms;
-//         },
-//         createForm: (args) => {
-//             const form = {
-//                 _id: Math.random().toString(),
-//                 name: args.formInput.name,
-//                 phoneNumber: args.formInput.phoneNumber,
-//                 email: args.formInput.email
-//             }
-//             forms.push(form);
-//             console.log(forms)
-//             return form;
-//         }
-//     },
-//     graphiql: true
-// }));
+        type RootQuery {
+            users: [String!]
+            forms: [Form!]!
+        }
+        type RootMutation {
+            createForm(formInput: FormInput): Form
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        forms: () => {
+            return forms;
+        },
+        createForm: (args) => {
+            const form = {
+                _id: Math.random().toString(),
+                name: args.formInput.name,
+                phoneNumber: args.formInput.phoneNumber,
+                email: args.formInput.email
+            }
+            forms.push(form);
+            console.log(forms)
+            return form;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(port);
