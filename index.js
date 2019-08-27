c543481b0e3f00c03d956a8bc0a967ae5d97e191
ECWireless <Elliott@valtechcreative.com>
2019-08-27 13:34:39 -0400

    small test

--- index.js
+++ index.js
@@ -6,56 +6,62 @@ const app = express();
 const { buildSchema } = require('graphql');
 const graphqlHttp = require('express-graphql');
 
+const PORT = process.env.PORT || 5000
+
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
 
 
 // Arrays
 
-let forms = [];
+// let forms = [];
 
-app.use('/graphql', graphqlHttp({
-    schema: buildSchema(`
-        type Form {
-            _id: ID!
-            name: String!
-            phoneNumber: String!
-            email: String!
-        }
+// app.use('/graphql', graphqlHttp({
+//     schema: buildSchema(`
+//         type Form {
+//             _id: ID!
+//             name: String!
+//             phoneNumber: String!
+//             email: String!
+//         }
 
-        input FormInput {
-            name: String!
-            phoneNumber: String!
-            email: String!
-        }
+//         input FormInput {
+//             name: String!
+//             phoneNumber: String!
+//             email: String!
+//         }
 
-        type RootQuery {
-            users: [String!]
-            forms: [Form!]!
-        }
-        type RootMutation {
-            createForm(formInput: FormInput): Form
-        }
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-        forms: () => {
-            return forms;
-        },
-        createForm: (args) => {
-            const form = {
-                _id: Math.random().toString(),
-                name: args.formInput.name,
-                phoneNumber: args.formInput.phoneNumber,
-                email: args.formInput.email
-            }
-            forms.push(form);
-            console.log(forms)
-            return form;
-        }
-    },
-    graphiql: true
-}));
+//         type RootQuery {
+//             users: [String!]
+//             forms: [Form!]!
+//         }
+//         type RootMutation {
+//             createForm(formInput: FormInput): Form
+//         }
+//         schema {
+//             query: RootQuery
+//             mutation: RootMutation
+//         }
+//     `),
+//     rootValue: {
+//         forms: () => {
+//             return forms;
+//         },
+//         createForm: (args) => {
+//             const form = {
+//                 _id: Math.random().toString(),
+//                 name: args.formInput.name,
+//                 phoneNumber: args.formInput.phoneNumber,
+//                 email: args.formInput.email
+//             }
+//             forms.push(form);
+//             console.log(forms)
+//             return form;
+//         }
+//     },
+//     graphiql: true
+// }));
